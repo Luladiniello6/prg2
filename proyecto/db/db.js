635f7553f4296b833834499361d13f202e015ce1
felixmilia <fmiliaotano@udesa.edu.ar>
2025-04-13 23:23:54 -0300

    Formularios

--- proyecto/db/db.js
+++ proyecto/db/db.js
@@ -17,47 +17,53 @@ const database = {
             id: 1,
             nombre: "Cafetera Moulinex",
             foto: "/images/img-cafetera-moulinex.jpg",
-            descripcion: "Descripcion"
+            descripcion: "Disfrutá del mejor café cada mañana con esta cafetera Moulinex de diseño compacto y funcional. Ideal para empezar el día con energía."
         },
         {
             id: 2,
             nombre: "Mac Book Pro",
             foto: "/images/img-macbook-pro-2019.jpg",
-            descripcion: "Descripcion"
+            descripcion: "Potencia y elegancia en una sola máquina. Este MacBook Pro 2019 ofrece un rendimiento profesional para tus tareas más exigentes."
+
         },
         {
             id: 3,
             nombre: "Samsung Galaxy S1",
             foto: "/images/img-samsung-galaxy-s10.jpg",
-            descripcion: "Descripcion"
+            descripcion: "Un smartphone con pantalla infinita, gran cámara y excelente rendimiento. El Galaxy S10 es tecnología de alto nivel en tu mano."
         },
         {
             id: 4,
             nombre: "Smart TV",
             foto: "/images/img-tv-samsung-smart.jpg",
-            descripcion: "Descripcion"
+            descripcion: "Viví la experiencia del cine en casa con esta Smart TV Samsung. Imagen nítida, sonido envolvente y acceso a tus apps favoritas."
         }
     ],
     comentarios: [
         {
             usuario: "user114",
             comentario: "Me encanto este producto",
-            imagen: ""
+            imagen: "/images/users/user1.webp"
         },
         {
             usuario: "julianalvarez",
             comentario: "Increible producto",
-            imagen: ""
+            imagen: "/images/users/julian.webp"
         },
         {
             usuario: "pablobarrios",
             comentario: "El envio fue muy lento y llego roto",
-            imagen: ""
+            imagen: "/images/users/user2.png"
         },
         {
             usuario: "rodrigo55",
             comentario: "El mejor producto que compre!",
-            imagen: ""
+            imagen: "/images/users/user3.avif"
+        },
+        {
+            usuario: "diego3",
+            comentario: "Buen producto",
+            imagen: "/images/users/user4.jpeg"
         }
     ]
 }
